9e91a665bfb69e852e3509705ddd500568ff2647
Vladislav Podchufarov <vxp032@cs.bham.ac.uk>
2021-08-24 20:43:16 +0100

    Update.

--- frontend/components/Users/ProfileHeader.tsx
+++ frontend/components/Users/ProfileHeader.tsx
@@ -1,9 +1,10 @@
+import { useRouter } from "next/router"
 
 export default function ProfileHeader() {
+  const router = useRouter()
   return (
     <div className="subheader">
-      <div className="wrapper">user1's sketches.</div>
+      <div className="wrapper"><b>{ router.query.username }</b>'s sketches.</div>
     </div>
   )
 }
-
